59ebaba67a29b25e5ea54823992a30fb5560f23a
root <emploi.neveu@gmail.com>
2018-11-27 11:35:21 +0100

    exo4

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <form action="user.php" method="post">
+    <p>prénom<input type="text" name="prénom" value=""></p>
+    <p>nom<input type="text" name="nom" value=""></p>
+    <button type="submit" name="button">Valider</button>
+    </form>
+  </body>
+</html>
---
+++ user.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <?php
+    echo $_POST['prénom'] . ' ' . $_POST['nom'];
+    ?>
+  </body>
+</html>
